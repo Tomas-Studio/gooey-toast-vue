4a15b2922ae71f8d93bab4fe5a392802ec634ad7
Olakunle Olatunji <tjomas20@gmail.com>
2026-04-13 05:40:17 +0100

    chore: style edits 2

--- demo/src/components/shared/ToggleSwitch.vue
+++ demo/src/components/shared/ToggleSwitch.vue
@@ -10,13 +10,13 @@ defineEmits<{
 
 <template>
   <button
-    class="relative w-10 h-[22px] rounded-full flex-shrink-0 transition-colors cursor-pointer"
+    class="relative w-10 h-5.5 rounded-full shrink-0 transition-colors cursor-pointer"
     :class="modelValue ? 'bg-green' : 'bg-card'"
     @click="$emit('update:modelValue', !modelValue)"
   >
     <span
-      class="absolute top-[3px] left-[3px] w-4 h-4 rounded-full bg-surface shadow-sm transition-transform"
-      :class="modelValue ? 'translate-x-[18px]' : ''"
+      class="absolute top-0.75 left-0.75 w-4 h-4 rounded-full bg-surface shadow-sm transition-transform"
+      :class="modelValue ? 'translate-x-4.5' : ''"
     />
   </button>
 </template>
