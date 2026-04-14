80c02e6524f135ea949a6e7f57366f3df590b8a6
Olakunle Olatunji <tjomas20@gmail.com>
2026-04-14 17:07:01 +0100

    style: format nuxt.ts

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -19,19 +19,15 @@ export default defineNuxtModule<GooeyToastNuxtOptions>({
   setup(options, nuxt) {
     const { resolve } = createResolver(import.meta.url)
 
-    // Add CSS
     nuxt.options.css.push('gooey-toast-vue/style.css')
 
-    // Transpile the library
     nuxt.options.build.transpile.push('gooey-toast-vue')
 
-    // Add plugin that registers GooeyToaster globally
     addPlugin({
       src: resolve('./runtime/plugin'),
       mode: 'client',
     })
 
-    // Auto-import composables and toast function
     if (options.autoImports) {
       addImports([
         {
